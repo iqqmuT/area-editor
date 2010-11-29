e6de4bb8163f57c9e18745f5510825e1b74d3c19
Arno Teigseth <arnotixe@gmail.com>
2010-11-29 12:45:55 -0500

    added more verbose data dumping to console, in the importJSON functions.

--- js/area.js
+++ js/area.js
@@ -29,6 +29,44 @@ console.warn = console.warn || function(){};
 console.error = console.error || function(){};
 console.info = console.info || function(){};
 
+
+/**
+ * Function : dump()
+ * Arguments: The data - array,hash(associative array),object
+ *    The level - OPTIONAL
+ * Returns  : The textual representation of the array.
+ * This function was inspired by the print_r function of PHP.
+ * This will accept some data as the argument and return a
+ * text that will be a more readable version of the
+ * array/hash/object that is given.
+ * Docs: http://www.openjs.com/scripts/others/dump_function_php_print_r.php
+ */
+function dump(arr,level) {
+	var dumped_text = "";
+	if(!level) level = 0;
+	
+	//The padding given at the beginning of the line.
+	var level_padding = "";
+	for(var j=0;j<level+1;j++) level_padding += "    ";
+	
+	if(typeof(arr) == 'object') { //Array/Hashes/Objects 
+		for(var item in arr) {
+			var value = arr[item];
+			
+			if(typeof(value) == 'object') { //If it is an array,
+				dumped_text += level_padding + "'" + item + "' ...\n";
+				dumped_text += dump(value,level+1);
+			} else {
+				dumped_text += level_padding + "'" + item + "' => \"" + value + "\"\n";
+			}
+		}
+	} else { //Stings/Chars/Numbers etc.
+		dumped_text = "===>"+arr+"<===("+typeof(arr)+")";
+	}
+	return dumped_text;
+}
+
+
 function initialize() {
 
   osm_map_type = new google.maps.ImageMapType({
@@ -571,14 +609,18 @@ function PoiManager() {
     for (var i in data) {
       var poi = data[i];
 
-// XXX arnos hack
-console.log("IN POI " + poi.id + '=' + poi.notes);
       this.add(new Poi(poi.id, '', '', poi.notes, new google.maps.LatLng(poi.latLng[0], poi.latLng[1])));
       c++;
     }
     if (poi_control.visible) {
       pois.show();
     }
+
+// XXX arnos hack
+// XXX arnos hack
+console.log(dump(data));
+
+
     return c;
   };
 
@@ -939,6 +981,12 @@ function AreaManager() {
     return arrayToJSON(this.areas);
   };
 
+
+
+
+
+
+
   // import areas from JSON data
   // returns integer of how many AREAs imported
   this.importJSON = function(data) {
@@ -953,15 +1001,20 @@ function AreaManager() {
           lng = area.path[j][1];
           path.push(new google.maps.LatLng(lat, lng));
       }
-// XXX arnos hack
-console.log("IN AREA " + area.name + '=' + area.number);
-
+// XXX the area-missing-number&name bug is here
       this.add(new Area(area.id, '', '', path));
       c++;
     }
     if (area_control.visible) {
       this.show();
     }
+
+
+
+// XXX arnos hack
+console.log(dump(data));
+
+
     return c;
   };
 
