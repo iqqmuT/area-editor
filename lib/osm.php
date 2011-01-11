678f15c225718ae25c7ed0a3a794be894466f96f
Tuomas Jaakola <tuomas.jaakola@iki.fi>
2011-01-11 13:54:36 -0500

    Forgot to add license text to lib/osm.php.

--- lib/osm.php
+++ lib/osm.php
@@ -1,4 +1,22 @@
 <?php
+/*
+ * Copyright 2011 Arno Teigseth, Tuomas Jaakola
+ * 
+ * This file is part of TOE.
+ *
+ * TOE is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * TOE is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with TOE.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 // This will create a OSM DOM that can be used for OSM export
 // Id's for nodes and ways will be re-generated
